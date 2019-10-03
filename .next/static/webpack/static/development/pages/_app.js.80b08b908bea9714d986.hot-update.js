108ebee42da1d3002634cd27ad00516dd1f6560a
scd7896 <scd1212@naver.com>
2019-10-03 15:42:21 +0900

    머지전어

---
+++ .next/static/webpack/static/development/pages/_app.js.80b08b908bea9714d986.hot-update.js
@@ -0,0 +1,44 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./componets/AppLayOut.js":
+/*!********************************!*\
+  !*** ./componets/AppLayOut.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _NavBar__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./NavBar */ "./componets/NavBar.js");
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+/* 이 파일이 보통 리액트를 시작할때 보는 
+    첫 화면 뿌려주기시작하는 단계로 보는것이 무방합니다
+    children에는 pages에 있는 index.js가 들어가서 뿌려지게됩니다.
+*/
+
+
+
+var AppLayOut = function AppLayOut(_ref) {
+  var children = _ref.children;
+  var style = {
+    zIndex: '1000'
+  };
+  return __jsx("div", null, __jsx(_NavBar__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    style: style
+  }), __jsx("div", {
+    style: {
+      zIndex: '0',
+      background: '#f5f7fa'
+    }
+  }, children));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (AppLayOut);
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.80b08b908bea9714d986.hot-update.js.map
---
+++ .next/static/webpack/static/development/pages/recruit.js.110c7e4ed5bbd60af111.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static/development/pages/recruit.js",{
+
+/***/ "./pages/recruit.js":
+/*!**************************!*\
+  !*** ./pages/recruit.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _css_kim_recruit_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/kim/recruit.scss */ "./css/kim/recruit.scss");
+/* harmony import */ var _css_kim_recruit_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_css_kim_recruit_scss__WEBPACK_IMPORTED_MODULE_1__);
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+var recruit = function recruit() {
+  return __jsx("div", {
+    id: "recruit_container"
+  }, __jsx("div", {
+    className: "container"
+  }, "\uD504\uB85C\uC81D\uD2B8\uCC3E\uAE30~~~", __jsx("h1", null, "\uD558\uD558\uD558")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (recruit);
+
+/***/ })
+
+})
+//# sourceMappingURL=recruit.js.110c7e4ed5bbd60af111.hot-update.js.map
