ad66fc78ceb38f53d23c5de803aaf4bdab1f8804
JunYoung <jun094@naver.com>
2019-10-05 15:34:01 +0900

    nav 및 웹폰트 구현

--- componets/NavBar.js
+++ componets/NavBar.js
@@ -7,21 +7,45 @@ const NavBar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src="" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="96"
+            height="50"
+            viewBox="0 0 96 50"
+          >
+            <text
+              id="Toys"
+              transform="translate(0 40)"
+              fill="#1f254b"
+              font-size="41"
+              font-family="Montserrat-Bold, Montserrat"
+              font-weight="700"
+            >
+              <tspan x="0" y="0">
+                Toys
+              </tspan>
+            </text>
+          </svg>
         </div>
-        
+
         <div className="nav_right">
           <span className="nav_list">
             <a href="/">모집중인 프로젝트</a>
             <a href="/find">프로젝트 후기</a>
           </span>
           <span className="nav_png">
-            <a href="#">
-              <img src="" />
-            </a>
-            <a href="#">
-              <img src="" />
-            </a>
+            <button>
+              <img
+                style={{ width: "25px", height: "32px" }}
+                src="https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/34B2916A-4DBF-46B6-94E2-B9F802EE2A0B.svg"
+              />
+            </button>
+            <button>
+              <img
+                style={{ width: "20px", height: "26px" }}
+                src="https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/31B34105-463E-4989-A29E-CE86470707C9.svg"
+              />
+            </button>
           </span>
           <span className="nav_button">
             <button>모집글 작성 ></button>
--- pages/_app.js
+++ pages/_app.js
@@ -1,55 +1,73 @@
-import React from 'react'
-import App from 'next/app'
-import Head from 'next/head'
-import withRedux from 'next-redux-wrapper'
-import {applyMiddleware, compose, createStore} from 'redux'
-import {Provider} from 'react-redux'
-import createSagaMiddleware from 'redux-saga'
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import withRedux from "next-redux-wrapper";
+import { applyMiddleware, compose, createStore } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
 
-import AppLayout from '../componets/AppLayOut'
+import AppLayout from "../componets/AppLayOut";
 
-import rootSaga from '../saga'
-import reducer from '../reducer'
+import rootSaga from "../saga";
+import reducer from "../reducer";
 
-import '../css/wrraper.scss'
-class MyApp extends App{
-    static async getInitialProps({Component, ctx}){
-        const pageProps = Component.getInitialProps
-        ? await Component.getInitialProps(ctx) : {}
-        //getInitialProps 가 서버사이드렌더링 효과를 줄수 있게 도와주는거에요
-        pageProps.query = ctx.query;
-        return {pageProps}
-    }
-    render(){
-        const {Component, pageProps, store} = this.props
-        return(
-            <Provider store = {store}>  
-            <Head>
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <meta charSet="utf-8" />
-              <link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css" />
-              <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
-              <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            </Head>
-  
-            <AppLayout>
-              <Component id = "wrraper" {...pageProps} />
-            </AppLayout>
-        </Provider>
-        )
-    }
+import "../css/wrraper.scss";
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+    //getInitialProps 가 서버사이드렌더링 효과를 줄수 있게 도와주는거에요
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Provider store={store}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <link
+            rel="stylesheet"
+            href="//cdn.quilljs.com/1.2.6/quill.snow.css"
+          />
+          {/* google web font Montserrat */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat"
+            rel="stylesheet"
+          ></link>
+          {/* google web font Noto Sans */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:300,400,500,700&display=swap&subset=korean"
+            rel="stylesheet"
+          ></link>
+
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        </Head>
+
+        <AppLayout>
+          <Component id="wrraper" {...pageProps} />
+        </AppLayout>
+      </Provider>
+    );
+  }
 }
-const configureStore = (initialState, option)=>{
-    const sagaMiddleware = createSagaMiddleware()
-    const middlewares = [sagaMiddleware]
-    const enhancer = process.env.NODE_ENV === 'production'
-    ? compose(applyMiddleware(...middlewares))
-    : compose(
-        applyMiddleware(...middlewares),
-        !option.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
-    )
-    const store = createStore(reducer, initialState, enhancer);
-    sagaMiddleware.run(rootSaga);
-    return store;
-}
-export default withRedux(configureStore)(MyApp)
+const configureStore = (initialState, option) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          !option.isServer &&
+            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+            ? window.__REDUX_DEVTOOLS_EXTENSION__()
+            : f => f
+        );
+  const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
+  return store;
+};
+export default withRedux(configureStore)(MyApp);
