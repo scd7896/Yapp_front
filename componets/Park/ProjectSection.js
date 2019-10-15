20c681e06669f0bfa9ab4410013ca5dec9f3f99a
JunYoung <jun094@naver.com>
2019-10-15 17:18:45 +0900

    최신등록글 section 반응형 완료

--- componets/Park/ProjectSection.js
+++ componets/Park/ProjectSection.js
@@ -1,38 +1,46 @@
-import ProjectCardView from '../../componets/Park/ProjectCardView';
+import ProjectCardView from "../../componets/Park/ProjectCardView";
 
-import '../../css/container.scss'
+import "../../css/container.scss";
 
-import '../../css/kim/index.scss'
+import "../../css/kim/index.scss";
 
 export default class ProjectSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [1, 2]
+    };
 
-    constructor(){
-        super();
-        this.state = {
-            data : [1,2,3,4,5,6]
-        }
+    this.handleMoreButton = this.handleMoreButton.bind(this);
+  }
 
-        this.handleMoreButton = this.handleMoreButton.bind(this);
-    }
+  handleMoreButton() {
+    var oldData = JSON.parse(JSON.stringify(this.state.data));
+    var newData = oldData.concat([1, 2]);
+    this.setState({ data: newData });
+  }
 
-    handleMoreButton(){
-        var oldData = JSON.parse(JSON.stringify(this.state.data)) ;
-        var newData = oldData.concat([1,2,3]);
-        this.setState({data:newData});
-    }
+  render() {
+    return (
+      <div id="project-section">
+        <div id="post_card_container">
+          {this.state.data.map((e, i) => {
+            return (
+              <div id="projectcard-flex">
+                <ProjectCardView />
+                <ProjectCardView />
+                <ProjectCardView />
+              </div>
+            );
+          })}
+        </div>
 
-    render(){
-        return (
-            <div id = "project-section">
-                <div id = "post_card_container">
-                    {this.state.data.map((e,i)=>{
-                        return <ProjectCardView />
-                    })}
-                </div>
-                <div id = "post_more_button_container">
-                    <button id = "post_more_button" onClick={this.handleMoreButton}>더보기</button>
-                </div>
-            </div>
-        )
-    }
+        <div id="post_more_button_container">
+          <button id="post_more_button" onClick={this.handleMoreButton}>
+            더보기
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
