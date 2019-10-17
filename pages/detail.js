6685296f616d709d0e76b03bddbefd778318fc22
sfsepark <sfsepark@gmail.com>
2019-10-17 21:45:59 +0900

    Detail_제목_양쪽정렬적용

--- pages/detail.js
+++ pages/detail.js
@@ -31,17 +31,22 @@ class Detail extends React.Component{
                             <div className = 'detail-title-jobgroups'>{jobgroups}</div>
                         </div>
                         <div className = 'detail-title-contents-wrapper'>
-                            <div className = 'detail-title'>{title}</div>
-                            <div className = "detail-button-wrapper">
-                                <div className = "detail-favorite-button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="17.854" height="25.768" viewBox="0 0 17.854 25.768">
-                                    <g id="그룹_1901" data-name="그룹 1901" transform="translate(-1450.769 -61)">
-                                        <path id="패스_1756" data-name="패스 1756" d="M1477.957,92.98v12.79h-.166l-8.582-6.594Z" transform="translate(-9.334 -19.003)" fill="#afafaf"/>
-                                        <path id="패스_1757" data-name="패스 1757" d="M1464.623,61V73.977l-8.749,6.2-9.106,6.452V61Z" transform="translate(4 0)" fill="#cccdd0"/>
-                                    </g>
-                                    </svg>
+                            <div className = 'detail-title-flex'>
+                                <div className = 'detail-title'>{title}</div>
+                                <div className = "detail-button-wrapper">
+                                    <div className = 'detail-button-flex'>
+                                    <div className = "detail-favorite-button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="17.854" height="25.768" viewBox="0 0 17.854 25.768">
+                                        <g id="그룹_1901" data-name="그룹 1901" transform="translate(-1450.769 -61)">
+                                            <path id="패스_1756" data-name="패스 1756" d="M1477.957,92.98v12.79h-.166l-8.582-6.594Z" transform="translate(-9.334 -19.003)" fill="#afafaf"/>
+                                            <path id="패스_1757" data-name="패스 1757" d="M1464.623,61V73.977l-8.749,6.2-9.106,6.452V61Z" transform="translate(4 0)" fill="#cccdd0"/>
+                                        </g>
+                                        </svg>
+                                    </div>
+                                    <div className = "detail-apply-button">지원하기</div>
+                                    </div>
+                                   
                                 </div>
-                                <div className = "detail-apply-button">지원하기</div>
                             </div>
                         </div>
                     </div>
@@ -74,7 +79,7 @@ class Detail extends React.Component{
 
                 <div className = 'detail-recruit-container container'>
                     <div className = 'detail-recruit-title  detail-block-title'>모집 직군</div>
-                    <JobGroupCardView type = 'big' jobgroup = 'designer'/>
+                    <JobGroupCardView type = 'big' jobgroup = 'designer' toggle = 'default'/>
                 </div>
 
                 <div className = 'detail-curmember-container container'>
