7802c8fb40b7987a6ed9eaa9409c63034459e3eb
scd7896 <scd1212@naver.com>
2019-10-19 15:17:41 +0900

    두번쨰 모달페이지

---
+++ componets/Kim/ApplySecond.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ApplySecond = ()=>{
+    return(
+        <div>
+            <div id = "first_modal_head_container">
+                <div>
+                    <img width = "100%"  src = "https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/D7793DDC-8712-4380-BDAD-CA942A68B083.svg"/>
+                </div>
+                <div id = "first_modal_head_text_container">
+                    <div id = "first_modal_head_icon_container">
+                        <p id = "first_modal_head_icon">2</p>
+                    </div>
+                    <p id = "first_modal_header_title">이력 뽐내기</p>
+                    <p id = "first_modal_header_subtitle">어필하고 싶은 이력이 있다면 넣어주세요!</p>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ApplySecond;
