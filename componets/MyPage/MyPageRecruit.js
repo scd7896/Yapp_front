e8de121bf4427a32d2df382811c4e445d2896bbe
sfsepark <sfsepark@gmail.com>
2019-10-20 17:23:41 +0900

    MyPage_상단섹션_퍼블리싱

---
+++ componets/MyPage/MyPageRecruit.js
@@ -0,0 +1,20 @@
+//마이페이지의 '모집 현황' 섹션입니다.
+
+import '../../css/Park/mypage.scss';
+import '../../css/MyPage/MyPageRecruit.scss';
+
+class MyPageRecruit extends React.Component{
+    
+    render(){
+        return (
+            <div className = 'mypage-bottom-section-bg'>
+                <div className = 'container'>
+                    <div className = 'mypage-sub-container'>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MyPageRecruit; 
