649b51e8ba88de56e0788db1080d80dc519c9aae
scd7896 <scd1212@naver.com>
2019-10-22 20:38:43 +0900

    로그인 입력폼 만들기

--- componets/Kim/Login/LoginInput.js
+++ componets/Kim/Login/LoginInput.js
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import '../../../css/kim/componentcss/Login/LoginInput.scss'
 const LoginInput = ()=>{
+    const [emailCheck, setEmailCheck] = useState(null)
+    const testEmail = (e)=>{
+        const check = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/
+        if(!check.test(e.target.value)){
+            setEmailCheck(false)
+        }else{
+            setEmailCheck(true)
+        }
+        
+    }
     return(
         <div>
-            <p><input type = "text" placeholder ="아이디(이메일형식)"/></p>
-            <p><input type = "password" placeholder ="비밀번호"/></p>
+            <p><input id= {emailCheck === null ? 'login_input_type': emailCheck ===false ? "login_input_type_fail": "login_input_type"} onChange = {testEmail} type = "text" placeholder ="아이디(이메일형식)"/></p>
+            <p><input id = "login_input_type" type = "password" placeholder ="비밀번호"/></p>
         </div>
     )
 }
