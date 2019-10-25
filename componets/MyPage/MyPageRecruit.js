71627e5a905113aead2127a47d892e3a90473a89
sfsepark <sfsepark@gmail.com>
2019-10-25 13:27:56 +0900

    Mypage_모집현황섹션

--- componets/MyPage/MyPageRecruit.js
+++ componets/MyPage/MyPageRecruit.js
@@ -3,6 +3,8 @@
 import '../../css/Park/mypage.scss';
 import '../../css/MyPage/MyPageRecruit.scss';
 
+import RecruitSimpleComponent from './RecruitSimpleComponent.js'
+
 class MyPageRecruit extends React.Component{
 
     constructor(props){
@@ -10,17 +12,39 @@ class MyPageRecruit extends React.Component{
 
         this.state = {
             recruit : [
-                
+                {
+                    id : 1,
+                    title : '프로젝트 타이틀1',
+                    jobgroup : '개발자',
+                    applicant : 3,
+                    finish : false
+                },
+                {
+                    id : 2,
+                    title : '프로젝트 타이틀2',
+                    jobgroup : '기획자',
+                    applicant : 20,
+                    finish : true
+                }
             ]
         }
     }
     
     render(){
+
+        var recruitSimpleComponents = this.state.recruit.map((info) => 
+            <RecruitSimpleComponent
+                info = {info}
+            />
+        );
+
+        console.log(recruitSimpleComponents);
+
         return (
             <div className = 'mypage-bottom-section-bg'>
                 <div className = 'container'>
                     <div className = 'mypage-sub-container mypage-recruit-container'>
-
+                        {recruitSimpleComponents}
                     </div>
                 </div>
             </div>
