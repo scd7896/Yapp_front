fbea2da55fd84340d3d23f6da118abe37f8bd99a
sfsepark <sfsepark@gmail.com>
2019-10-26 15:18:48 +0900

    Mypage_지원현황
    component HOC화, 지원취소 더미 기능 구현

--- componets/MyPage/ApplySimpleContents.js
+++ componets/MyPage/ApplySimpleContents.js
@@ -2,8 +2,26 @@ import '../../css/MyPage/ProjectSimpleContainer.scss'
 
 var ApplySimpleContents = (props) =>
 {
+    var reading = props.info.reading;
+    var finish = props.info.finish;
+
     return (
-        <div/>
+        <div className = 'apply-simple-container'>
+            <div className = 'apply-simple-flex'>
+                <div className = {'apply-simple-reading-status '+ 'apply-simple-reading-' + (reading ? 'true' : 'false')}>
+                    {reading ? '열람' : '미열람'}
+                </div>
+                <div className = {'apply-simple-progress-status ' + 'apply-simple-progress-' + (finish ? 'finish' : 'in')}>
+                    {finish ? '모집완료' : '모집중'}
+                </div>
+                <div className = 'apply-simple-cancel-button' onClick = {() => props.onDelete(props.id)}>
+                    <div className = 'apply-simple-cancel-button-text'>
+                        지원취소
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
