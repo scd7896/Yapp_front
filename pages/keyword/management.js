0d181fedf6da4813474951a6f6636818e28ee72f
scd7896 <scd1212@naver.com>
2019-11-02 14:45:52 +0900

    상단 퍼블리싱

--- pages/keyword/management.js
+++ pages/keyword/management.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import '../../css/container.scss'
+import '../../css/kim/keyword_management.scss'
 const management = ()=>{
     return(
-        <div style = {{background : "#ffffff"}}>
-            <div className = "container">
-                <div>
-                    <p>키워드 관리</p>
+        <div>
+            <div style = {{background : "#ffffff", borderBottom : "3px solid #f5f7fa"}}>
+                <div className = "container">
+                    <div className = "management_container">
+                        <div>
+                            <p className = "keyword_management_text">키워드 관리</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    바디
+            </div>
+            <div style = {{background : "#ffffff"}}>
+            <div className = "container">
+                    <div className = "management_container">
+                        <div>
+                            <span className = "keyword_management_body_text">관심키워드 선택하고 </span>
+                            <br/>
+                            <span className = "keyword_management_body_text">메인페이지에서 만나보세요!</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
