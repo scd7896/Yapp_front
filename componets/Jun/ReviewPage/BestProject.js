ed26f50fd36fde130e935f375b2dc8d323caf8d0
JunYoung <jun094@naver.com>
2019-11-09 13:56:14 +0900

    BestProject 컴포넌트 수정

--- componets/Jun/ReviewPage/BestProject.js
+++ componets/Jun/ReviewPage/BestProject.js
@@ -8,11 +8,6 @@ function BestProject() {
         <div className="hoverBox"> </div>
         <img src="https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/B57E5EE6-9C76-4723-9D0F-F1EFE29CABC6.png" />
       </div>
-
-      <div className="bestProject_2" id="positionRelative">
-        <div className="hoverBox"> </div>
-        <img src="https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/B57E5EE6-9C76-4723-9D0F-F1EFE29CABC6.png" />
-      </div>
     </div>
   );
 }
