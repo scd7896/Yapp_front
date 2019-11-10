fef903f4735cdc222cfa6195d07c1eb302786e84
scd7896 <scd1212@naver.com>
2019-11-10 16:14:45 +0900

    ree

--- pages/_error.js
+++ pages/_error.js
@@ -1,16 +1,25 @@
 import Error from 'next/error'
 import React from 'react'
+import Link from 'next/link'
 import ErrorCode from '../componets/Kim/error/atomic/ErrorCode'
 import ErrorContents from '../componets/Kim/error/atomic/ErrorContents'
 import ErrorImage from '../componets/Kim/error/atomic/ErrorImage'
 import '../css/kim/_error.scss'
 const MyError = ({statusCode})=>{
+    
     return(
         <div className = "error_page_container">
             
             <div className = "error_page_left_container">
                 <ErrorCode errorStatus = {statusCode}/>
                 <ErrorContents errorStatus = {statusCode}/>
+                <Link href = '/'>
+                    <div className = "error_page_home_button">
+                        <p className = "error_page_home_button_text">홈으로 이동</p>
+                        <img src = "https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/CA153ACE-A9AF-4405-A002-A2800E77AA69.svg"/>
+                    </div>
+                </Link>
+                
             </div>
             <div>
                 <ErrorImage errorStatus = {statusCode}/>
