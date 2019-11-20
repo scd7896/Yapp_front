657b6d3a381584eb435d638a108f7150836e61d1
scd7896 <scd1212@naver.com>
2019-11-21 01:13:35 +0900

    질문 리스트 퍼블리싱 완료

--- componets/Kim/enrollment/atomic/QuestionLine.js
+++ componets/Kim/enrollment/atomic/QuestionLine.js
@@ -21,9 +21,10 @@ const QuestionLine = ({position, jindex, text})=>{
     }
     return(
         <div className = "question_line_container">
-            <div>Q</div>
-            <div>{jindex + 1}</div>
-            <input type = "text" value = {text} onChange = {changeText}/>
+            <div className = "question_text_q">Q</div>
+            <div className = "question_text_number">{jindex + 1}</div>
+            <input className = "quesition_text_input" type = "text" value = {text} onChange = {changeText}
+                placeholder = "질문을 입력하세요 ex)성실히 참여 할 수 있나요?"/>
             <div className = {jindex ===0 ?"display_none":""} onClick = {removeQuestionList}>
                 <img src = "https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/05E60A73-F0A9-4ED5-9684-62DD71155A8A.svg"/>
             </div>
