df628883d26ee93f6bb2d062c8cc9efd918307cc
sfsepark <sfsepark@gmail.com>
2019-11-22 15:37:09 +0900

    Detail_우측상단 버튼 컴포넌트화
    user 정보에 대한 렌더링을 최소화하기위해서 따로 함수형 컴포넌트로 추출함

--- componets/detail.js
+++ componets/detail.js
@@ -8,6 +8,7 @@ import JobGroupCardView from './Park/JobGroupCardView'
 import DetailQnA from './Park/DetailQnA'
 import role from '../methods/role'
 import ProjectJobGroup from './Park/ProjectJobGroup'
+import DetailButtons from './Park/DetailButtons'
 
 import cookies from '../methods/cookies'
 
@@ -29,15 +30,8 @@ class Detail extends React.Component{
         }
 
         this.projectId = this.props.query.id;
-        this. handleClickApplyButton = this. handleClickApplyButton.bind(this);
     }
 
-    handleClickFavorite(){
-    }
-
-    fetchIsFavorite(){
-      
-    }
 
     componentDidMount(){
 
@@ -55,15 +49,6 @@ class Detail extends React.Component{
       })
     }
 
-    handleClickApplyButton(){
-      var userToken = cookies.getCookie('user-token');
-      if(userToken == undefined || userToken == ''){
-          this.props.openLoginModal();
-      }
-      else{
-        this.props.openApplyModal()
-      }
-    }
     
     render(){
         
@@ -84,29 +69,6 @@ class Detail extends React.Component{
             </span>
           )
         );
-
-        var buttons = (
-          <div className = 'detail-button-flex'>
-            <div className = "detail-favorite-button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="17.854" height="25.768" viewBox="0 0 17.854 25.768">
-                <g id="그룹_1901" data-name="그룹 1901" transform="translate(-1450.769 -61)">
-                    <path id="패스_1756" data-name="패스 1756" d="M1477.957,92.98v12.79h-.166l-8.582-6.594Z" transform="translate(-9.334 -19.003)" fill="#afafaf"/>
-                    <path id="패스_1757" data-name="패스 1757" d="M1464.623,61V73.977l-8.749,6.2-9.106,6.452V61Z" transform="translate(4 0)" fill="#cccdd0"/>
-                </g>
-                </svg>
-            </div>
-            <div className = "detail-apply-button" onClick = {this.handleClickApplyButton}>지원하기</div>
-          </div>
-        )
-        console.log(this.props.user.userId);
-
-        if(this.props.project && this.props.user.userId && 
-          this.props.project.userId == this.props.userId){
-            <div className = 'detail-button-flex'>
-            <div className = "detail-apply-button">수정</div>
-              <div className = "detail-apply-button">모집마감</div>
-            </div>
-        }
         
         return(
             <div id = 'detail_root'>
@@ -127,7 +89,9 @@ class Detail extends React.Component{
                             <div className = 'detail-title-flex'>
                                 <div className = 'detail-title'>{title}</div>
                                 <div className = "detail-button-wrapper">
-                                  {buttons}
+                                  <DetailButtons
+                                    projectId = {this.projectId}
+                                    projectUserId = {this.props.project.userId}/>
                                 </div>
                             </div>
                         </div>
