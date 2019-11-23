c7eb75ac65aeab6ed293037eca7bba587c1eaa8e
sfsepark <sfsepark@gmail.com>
2019-11-23 17:31:09 +0900

    Footer_퍼블리싱

---
+++ componets/Footer.js
@@ -0,0 +1,20 @@
+import '../css/footer.scss'
+
+export default function Footer(){
+    return (
+        <div id  = "footer">
+            <div id = "footer-contents">
+                <div id = "footer-flex">
+                <div id = "footer-logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="68" height="35" viewBox="0 0 68 35">
+                <text id="Toys" transform="translate(0 28)" fill="#4ce4bd" font-size="29" font-family="Montserrat-Bold, Montserrat" font-weight="700"><tspan x="0" y="0">Toys</tspan></text>
+                </svg>
+                </div>
+                <div id = 'footer-copyright'>
+                Copyright (c) 2019 Toys team All rights reserved.
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
