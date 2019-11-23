6a83b9a7ec81867bdd0bdabf2ae70af99db04326
JunYoung <jun094@naver.com>
2019-11-23 17:28:03 +0900

    ApplyModal 질문 리스트 반환

--- reducer/applyModal.js
+++ reducer/applyModal.js
@@ -1,27 +1,27 @@
-import produce from "immer";
-import {
-    PROJECT_APPLY_SUCCESS,
-    PROJECT_APPLY_FAILURE,
-    PROJECT_APPLY_REQUEST
-} from "../action";
-import Router from "next/router";
+// import produce from "immer";
+// import {
+//     PROJECT_APPLY_SUCCESS,
+//     PROJECT_APPLY_FAILURE,
+//     PROJECT_APPLY_REQUEST
+// } from "../action";
+// import Router from "next/router";
 
-const initialProps = {
-    selectList: [],
-    resId: null
-};
+// const initialProps = {
+//     selectList: [],
+//     resId: null
+// };
 
-const enorollment = (state = initialProps, action) => {
-    return produce(state, draft => {
-        switch (action.type) {
-            case PROJECT_APPLY_SUCCESS:
-                draft.resId = action.data;
-                setStep(2);
-                break;
-            default:
-                break;
-        }
-    });
-};
+// const enorollment = (state = initialProps, action) => {
+//     return produce(state, draft => {
+//         switch (action.type) {
+//             case PROJECT_APPLY_SUCCESS:
+//                 draft.resId = action.data;
+//                 setStep(2);
+//                 break;
+//             default:
+//                 break;
+//         }
+//     });
+// };
 
-export default enrollment;
+// export default enrollment;
