4842a69a651afc6a910853b3402f0a5e231c3ded
sfsepark <sfsepark@gmail.com>
2019-11-25 13:51:49 +0900

    프로젝트 카드뷰 렌더링

--- componets/Park/ProjectCardView.js
+++ componets/Park/ProjectCardView.js
@@ -3,35 +3,27 @@ import Router from 'next/router'
 import "../../css/Park/ProjectCardView.scss";
 import ProjectJobGroup from "./ProjectJobGroup";
 import Link from 'next/link';
+import getTimeDiff from '../../methods/getTimeDiff'
+import steps from '../../methods/step';
+import roles from  '../../methods/role';
+import locations from '../../methods/location'
 
 const ProjectCardView = props => {
 
-  var jobgroup_main = "개발자",
-    jobgroup_sub = "프론트,백엔드",
-  
-    step = "팀 빌딩 단계",
-    region = "서울",
-    time = "5시간 전";
+  var jobgroup_main = "JOBGROUP_MAIN",
+    role = "ROLE",
+    step = "STEP",
+    region = "REGION",
+    time = "TIME";
 
   var title = (props.project ? props.project.title : '');
   
-  if (props != undefined && props.data != undefined) {
-    jobgroup_main = props.data.jobgroup[0];
-
-    jobgroup_sub = "";
-
-    for (let i = 1; i < props.data.jobgroup.length; i++) {
-      jobgroup_sub += props.data.jobgroup[i] + ",";
-    }
-
-    if (jobgroup_sub.length > 0) {
-      jobgroup_sub = jobgroup_sub.substr(0, jobgroup_sub.length - 1);
-    }
-
-    title = props.data.title;
-    step = props.data.step;
-    region = props.data.region;
-    time = props.data.time;
+  if (props != undefined && props.project != undefined) {
+    region = locations[props.project.location]
+    time = getTimeDiff(props.project.createAt);
+    step = steps[props.project.step]
+    title = props.project.title;
+    role = roles[props.project.role];
   }
   const moveToDetailPage = ()=>{
     if(props.project){
