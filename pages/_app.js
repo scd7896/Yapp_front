ab172f6047b19d8e887547d48ae58b8ec2de2ff4
scd7896 <scd1212@naver.com>
2019-11-27 00:32:56 +0900

    넥스트 리덕스 사가 붙이기

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import React from "react";
-import App from "next/app";
+import App,{Container} from "next/app";
 import Head from "next/head";
 import withRedux from "next-redux-wrapper";
 import withReduxSaga from 'next-redux-saga'
 import { applyMiddleware, compose, createStore } from "redux";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import cookies from 'next-cookies'
 import axios from 'axios'
@@ -16,11 +16,33 @@ import reducer from "../reducer";
 import "../css/wrraper.scss";
 import url from '../url'
 import { GET_MYDATA_REQUEST, GET_MYDATA_SUCCESS, GET_MYDATA_FAILURE } from "../action";
+
+const configureStore = (initialState, option) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          !option.isServer &&
+            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+            ? window.__REDUX_DEVTOOLS_EXTENSION__()
+            : f => f
+        );
+  const store = createStore(reducer, initialState, enhancer);
+  if(option.req || !option.isServer){
+    store.sagaTask = sagaMiddleware.run(rootSaga)
+  }
+  
+  return store;
+};
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
-    const pageProps = Component.getInitialProps
-      ? await Component.getInitialProps(ctx)
-      : {};
+    let pageProps = {}
+    if(Component.getInitialProps){
+      pageProps = await Component.getInitialProps(ctx)
+    }
     //getInitialProps 가 서버사이드렌더링 효과를 줄수 있게 도와주는거에요
     pageProps.query = ctx.query;
     const userToken = cookies(ctx)['user-token']
@@ -54,45 +76,30 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Provider store={store}>
-        <Head>
-          <meta
-            content="width=device-width,minimum-scale=1.0"
-            name="viewport"
-          />
-          <meta charSet="utf-8" />
-          {/* google web font Montserrat */}
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"/>
-          {/* google web font Noto Sans */}
-          <link
-            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
-            rel="stylesheet"
-          ></link>
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        </Head>
+      <Container>
+        <Provider store={store}>
+          <Head>
+            <meta
+              content="width=device-width,minimum-scale=1.0"
+              name="viewport"
+            />
+            <meta charSet="utf-8" />
+            {/* google web font Montserrat */}
+            <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"/>
+            {/* google web font Noto Sans */}
+            <link
+              href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
+              rel="stylesheet"
+            ></link>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+          </Head>
 
-        <AppLayout>
-          <Component id="wrraper" {...pageProps} />
-        </AppLayout>
-      </Provider>
+          <AppLayout>
+            <Component id="wrraper" {...pageProps} />
+          </AppLayout>
+        </Provider>
+      </Container>
     );
   }
 }
-const configureStore = (initialState, option) => {
-  const sagaMiddleware = createSagaMiddleware();
-  const middlewares = [sagaMiddleware];
-  const enhancer =
-    process.env.NODE_ENV === "production"
-      ? compose(applyMiddleware(...middlewares))
-      : compose(
-          applyMiddleware(...middlewares),
-          !option.isServer &&
-            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
-            ? window.__REDUX_DEVTOOLS_EXTENSION__()
-            : f => f
-        );
-  const store = createStore(reducer, initialState, enhancer);
-  sagaMiddleware.run(rootSaga);
-  return store;
-};
-export default withRedux(configureStore)((MyApp));
+export default withRedux(configureStore)(withReduxSaga(MyApp)) ;
