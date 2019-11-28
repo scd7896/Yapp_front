14912ece5335861e308c109c23a2971365bbaea1
JunYoung <jun094@naver.com>
2019-11-29 00:57:24 +0900

    ApplyModalCompleted 수정

--- saga/index.js
+++ saga/index.js
@@ -3,10 +3,15 @@ import user from "./user";
 import keywords from "./keywords";
 import join from "./join";
 import enrollment from "./enrollment";
-import favorite from './favorite'
-
+import favorite from "./favorite";
 
 /* 비동기적으로 리듀서에 저장해야하는 데이터를 요청하는 부분을 올릴 예정입니다. */
 export default function* rootSaga() {
-  yield all([fork(user), fork(join), fork(keywords), fork(enrollment),fork(favorite)]);
+  yield all([
+    fork(user),
+    fork(join),
+    fork(keywords),
+    fork(enrollment),
+    fork(favorite)
+  ]);
 }
