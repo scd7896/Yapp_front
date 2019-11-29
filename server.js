d5ec0e43412a268485151e999f9a7601b56a7f02
scd7896 <scd1212@naver.com>
2019-11-29 22:44:15 +0900

    모건 적용

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require('express')
 const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
+const fs = require('fs')
 const handle = app.getRequestHandler()
+const path = require('path')
 const prod = process.env.NODE_ENV === 'production';
+const morgan = require('morgan')
+const uuid = require('uuid')
+
+morgan.token('id', function getId(req, res){
+    return req.id
+})
+function assignId (req, res, next) {
+    req.id = uuid.v4()
+    next()
+  }
 app.prepare().then(()=>{
     const server = express()
     server.use(express.json())
     server.use(express.urlencoded({extended: true}))
+    // server.use(assignId)
+    // server.use(morgan('common',{
+    //     stream : fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+    // }))
+    
     server.get('/keyword/management', (req,res)=>{
         return app.render(req,res, '/keyword/management')
     })
