4861e04c89829be8d0131db58c1cd06597706a11
JunYoung <jun094@naver.com>
2019-11-30 04:27:09 +0900

    recruit페이지, enrollment페이지 css수정

--- componets/FindSelectbox.js
+++ componets/FindSelectbox.js
@@ -1,44 +1,47 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../css/FindSelectbox.scss";
 import "../css/container.scss";
-import {useSelector} from 'react-redux'
-import SelectBox from './Jun/SelectBox'
-import locations from '../methods/location'
-import url from '../url'
+import { useSelector } from "react-redux";
+import SelectBox from "./Jun/SelectBox";
+import locations from "../methods/location";
+import url from "../url";
 import axios from "axios";
-const FindSelectbox = ({onSubmit}) => {
-  const {projectKeyword} = useSelector(state=> state.enrollment)
-  const [searchString, setSearchString] = useState(null)
+const FindSelectbox = ({ onSubmit }) => {
+  const { projectKeyword } = useSelector(state => state.enrollment);
+  const [searchString, setSearchString] = useState(null);
   const [inputs, setInputs] = useState({
-        location : {id : null, text : null}
-  })
-  const {location} = inputs
-  const locationsItems = locations.map((el,i)=>{
-    return {id : i, text : el}
-  })
-  const onClick = e =>{
-      const {name, value} = e;
-      setInputs({
-          ...inputs,
-          [name] : {id : value.id, text : value.text}
-      })
-  }
-  const stringSet = (e)=>{
-    setSearchString(e.target.value)
-    
-  }
-  const submitSearch = async()=>{
-    
-    const loactionId = inputs.location.id
-    
-    
-    let baseUrl = url+"/projects/search" +"?" + (searchString? `term=${searchString}&`: "");
-    baseUrl += (loactionId!==null? `location=${loactionId}`:"")
+    location: { id: null, text: null }
+  });
+  const { location } = inputs;
+  const locationsItems = locations.map((el, i) => {
+    return { id: i, text: el };
+  });
+  const onClick = e => {
+    const { name, value } = e;
+    setInputs({
+      ...inputs,
+      [name]: { id: value.id, text: value.text }
+    });
+  };
+  const stringSet = e => {
+    setSearchString(e.target.value);
+  };
+  const submitSearch = async () => {
+    const loactionId = inputs.location.id;
 
-    const result = await axios.post(encodeURI(baseUrl) ,{"keywords" : projectKeyword}).catch((err)=> console.log(err))
-    
-    onSubmit(result.data)
-  }
+    let baseUrl =
+      url +
+      "/projects/search" +
+      "?" +
+      (searchString ? `term=${searchString}&` : "");
+    baseUrl += loactionId !== null ? `location=${loactionId}` : "";
+
+    const result = await axios
+      .post(encodeURI(baseUrl), { keywords: projectKeyword })
+      .catch(err => console.log(err));
+
+    onSubmit(result.data);
+  };
   return (
     <div className="findProject">
       <div className="text_find">
@@ -46,27 +49,33 @@ const FindSelectbox = ({onSubmit}) => {
           style={{ width: "31px", height: "35px" }}
           src="https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/0A4A76EC-FF35-4D30-A469-340964DF83ED.svg"
         />
-        <input style={{paddingBottom: "20px"}} value = {searchString} placeholder="검색어 입력" onChange = {stringSet}></input>
+        <input
+          style={{ paddingBottom: "20px" }}
+          value={searchString}
+          placeholder="검색어 입력"
+          onChange={stringSet}
+        ></input>
       </div>
-      <hr id = "findbox_hr_line"/>
+      <hr id="findbox_hr_line" />
       <div className="text_find">
         <img
           style={{ width: "31px", height: "35px" }}
           src="https://cdn.zeplin.io/5d8afd2a43adab15d5458ff0/assets/0A4A76EC-FF35-4D30-A469-340964DF83ED.svg"
         />
-        <div id = "recruit_select_box">
-          <SelectBox 
-            name = "location"
-            value = {location.text}
-            type = "under"
+        <div id="recruit_select_box">
+          <SelectBox
+            name="location"
+            value={location.text}
+            type="under"
             placeholder="지역 선택"
-            items = {locationsItems}
-            inputs = {inputs}
-            onClick = {onClick}/>
+            items={locationsItems}
+            inputs={inputs}
+            onClick={onClick}
+          />
         </div>
       </div>
 
-      <button onClick = {submitSearch}>검색</button>
+      <button onClick={submitSearch}>검색</button>
     </div>
   );
 };
