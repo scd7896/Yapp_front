c148f8dc87d12787672535a248632575ff2b777d
JunYoung <jun094@naver.com>
2019-11-30 12:44:49 +0900

    Auto stash before merge of "master" and "origin/master"

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,28 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import withRedux from "next-redux-wrapper";
-import withReduxSaga from 'next-redux-saga'
+import withReduxSaga from "next-redux-saga";
 import { applyMiddleware, compose, createStore } from "redux";
 import { Provider, connect } from "react-redux";
 import createSagaMiddleware from "redux-saga";
-import cookies from 'next-cookies'
-import axios from 'axios'
+import cookies from "next-cookies";
+import axios from "axios";
 import AppLayout from "../componets/AppLayOut";
 
 import rootSaga from "../saga";
 import reducer from "../reducer";
 
-import mobileCheck from '../methods/mobileCheck'
+import mobileCheck from "../methods/mobileCheck";
 
 import "../css/wrraper.scss";
-import "../css/Mypage/MyPortfolioSection.scss"
+//import "../css/MyPortfolioSection.scss/"
 
-import url from '../url'
-import { GET_MYDATA_REQUEST, GET_MYDATA_SUCCESS, GET_MYDATA_FAILURE } from "../action";
+import url from "../url";
+import {
+  GET_MYDATA_REQUEST,
+  GET_MYDATA_SUCCESS,
+  GET_MYDATA_FAILURE
+} from "../action";
 
 const configureStore = (initialState, option) => {
   const sagaMiddleware = createSagaMiddleware();
@@ -35,56 +39,52 @@ const configureStore = (initialState, option) => {
             : f => f
         );
   const store = createStore(reducer, initialState, enhancer);
-  if(option.req || !option.isServer){
-    store.sagaTask = sagaMiddleware.run(rootSaga)
+  if (option.req || !option.isServer) {
+    store.sagaTask = sagaMiddleware.run(rootSaga);
   }
-  
+
   return store;
 };
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
-
-
-
-
-    let pageProps = {}
-    if(Component.getInitialProps){
-      pageProps = await Component.getInitialProps(ctx)
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
     //getInitialProps 가 서버사이드렌더링 효과를 줄수 있게 도와주는거에요
     pageProps.query = ctx.query;
-    const userToken = cookies(ctx)['user-token']
+    const userToken = cookies(ctx)["user-token"];
     const me = ctx.store.getState().user;
     if (!me.userId) {
-      try{
-        const result = await axios.get(`${url}/me`,{
-          headers : {
-              Authorization: `bearer ${userToken}`
+      try {
+        const result = await axios.get(`${url}/me`, {
+          headers: {
+            Authorization: `bearer ${userToken}`
           }
-        })
-        
-        ctx.store.dispatch({
-          type : GET_MYDATA_SUCCESS,
-          userToken : userToken,
-          userEmail : result.data.user.email,
-          userId : result.data.user.userId,
-          userName : result.data.user.name,
-          userProfileImage : result.data.user.profileImage
-        })
+        });
 
-      }catch(e){
         ctx.store.dispatch({
-          type : GET_MYDATA_FAILURE
-        })
+          type: GET_MYDATA_SUCCESS,
+          userToken: userToken,
+          userEmail: result.data.user.email,
+          userId: result.data.user.userId,
+          userName: result.data.user.name,
+          userProfileImage: result.data.user.profileImage
+        });
+      } catch (e) {
+        ctx.store.dispatch({
+          type: GET_MYDATA_FAILURE
+        });
       }
     }
 
-    const userAgent = ctx.req ?  ctx.req.headers['user-agent'] : navigator.userAgent
+    const userAgent = ctx.req
+      ? ctx.req.headers["user-agent"]
+      : navigator.userAgent;
 
-    if(mobileCheck(userAgent)){
+    if (mobileCheck(userAgent)) {
       pageProps.isMobile = true;
-    }
-    else{
+    } else {
       pageProps.isMobile = false;
     }
 
@@ -93,24 +93,26 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      
-        <Provider store={store}>
-          <Head>
-            <meta
-              content="width=device-width,minimum-scale=1.0"
-              name="viewport"
-            />
-            <meta charSet="utf-8" />
-            {/* google web font Montserrat */}
-            <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"/>
-            {/* google web font Noto Sans */}
-            <link
-              href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
-              rel="stylesheet"
-            ></link>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script src = "https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
-          </Head>
+      <Provider store={store}>
+        <Head>
+          <meta
+            content="width=device-width,minimum-scale=1.0"
+            name="viewport"
+          />
+          <meta charSet="utf-8" />
+          {/* google web font Montserrat */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
+            rel="stylesheet"
+          />
+          {/* google web font Noto Sans */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
+            rel="stylesheet"
+          ></link>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
+        </Head>
 
           <AppLayout isMobile = {pageProps.isMobile}>
             <Component id="wrraper" {...pageProps} />
@@ -120,4 +122,4 @@ class MyApp extends App {
     );
   }
 }
-export default withRedux(configureStore)(withReduxSaga(MyApp)) ;
+export default withRedux(configureStore)(withReduxSaga(MyApp));
