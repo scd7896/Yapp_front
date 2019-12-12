a79938adda166de0c95abb2bb9ff628bb2fb9a1e
sfsepark <sfsepark@gmail.com>
2019-12-12 20:09:25 +0900

    키워드 오타 수정

--- methods/keywords.js
+++ methods/keywords.js
@@ -1,2 +1,2 @@
 export default ["웹서비스", "iOS", "ANDROID", "UX/UI", "디자인","개발", "기획",
-"AI", "IoT", "PHYTHON", "프론트", "백엔드","GAME", "공모전", "해커톤"]
+"AI", "IoT", "PYTHON", "프론트", "백엔드","GAME", "공모전", "해커톤"]
