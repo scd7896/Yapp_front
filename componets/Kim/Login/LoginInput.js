2dab27800561fab08fb3bbcaec2b9896204e0c99
scd7896 <scd1212@naver.com>
2019-12-13 16:04:37 +0900

    엔터 로그인 구현

--- componets/Kim/Login/LoginInput.js
+++ componets/Kim/Login/LoginInput.js
@@ -31,7 +31,7 @@ const LoginInput = ()=>{
         })
     }
     const loginRequest = (event)=>{
-        
+        event.preventDefault();
         if(emailCheck !== true){
             return;
         }
@@ -49,34 +49,27 @@ const LoginInput = ()=>{
         setEmailCheck(null)
     }
 
-    useEffect(()=>{
-        const container = document.querySelector("#login_container")
-
-        container.addEventListener('keypress', (event)=>{
-            if(event.keyCode ===13){
-                //console.log(emailCheck)
-                loginRequest()
-            }
-            
-        })
-    },[])
     return(
-        <div id = "login_container">            
-            <p><input id = "login_email" style ={{marginLeft : '-5%'}} className= {emailCheck === null ? 'login_input_type': emailCheck ===false ? "login_input_type_fail": "login_input_type"} onChange = {testEmail} type = "text" placeholder ="아이디(이메일형식)"/></p>
-            <p style = {{textAlign:"left", paddingLeft : '3%'}}>
-                <span className = {emailCheck === false ? 'login_email_check_error' : 'display_none'}>이메일 형식으로 입력해주세요</span>
-            </p>    
-            <p className = {emailCheck === true ? "login_email_check_good" : "display_none"}><svg xmlns="http://www.w3.org/2000/svg" width="28.901" height="19.83" viewBox="0 0 28.901 19.83">
-  <path id="패스_1805" data-name="패스 1805" d="M1946.841,3691.941l9.779,9.779,17.708-17.708" transform="translate(-1946.134 -3683.304)" fill="none" stroke="#1f254b" stroke-width="2"/>
-</svg></p>
-            <p><input id = "login_password" onChange = {repairPassword} style ={{marginLeft : '-5%'}} className = "login_input_type" type = "password" placeholder ="비밀번호"/></p>
-            <p style = {{textAlign:"left", paddingLeft : '3%'}}>
-                <span className = {loginFail === true ? 'login_email_check_error' : 'display_none'}>아이디/비밀번호가 일치하지 않습니다</span>
-            </p>
-            
-            
+        <div id = "login_container">      
+            <form onSubmit = {loginRequest}>      
+                <p><input id = "login_email" style ={{marginLeft : '-5%'}} className= {emailCheck === null ? 'login_input_type': emailCheck ===false ? "login_input_type_fail": "login_input_type"} onChange = {testEmail} type = "text" placeholder ="아이디(이메일형식)"/></p>
+                <p style = {{textAlign:"left", paddingLeft : '3%'}}>
+                    <span className = {emailCheck === false ? 'login_email_check_error' : 'display_none'}>이메일 형식으로 입력해주세요</span>
+                </p>    
+                <p className = {emailCheck === true ? "login_email_check_good" : "display_none"}><svg xmlns="http://www.w3.org/2000/svg" width="28.901" height="19.83" viewBox="0 0 28.901 19.83">
+                    <path id="패스_1805" data-name="패스 1805" d="M1946.841,3691.941l9.779,9.779,17.708-17.708" transform="translate(-1946.134 -3683.304)" fill="none" stroke="#1f254b" stroke-width="2"/>
+                    </svg>
+                </p>
+                <p><input id = "login_password" onChange = {repairPassword} style ={{marginLeft : '-5%'}} className = "login_input_type" type = "password" placeholder ="비밀번호"/></p>
+                <p style = {{textAlign:"left", paddingLeft : '3%'}}>
+                    <span className = {loginFail === true ? 'login_email_check_error' : 'display_none'}>아이디/비밀번호가 일치하지 않습니다</span>
+                </p>
+                <button className = "login_request_button" ><p className = "login_request_text">로그인</p></button>
+            </form>
+
+
             <div className = "login_modal_footer_container">
-                <div className = "login_request_button" onClick = {loginRequest}><p className = "login_request_text">로그인</p></div>
+                
                 <div className = "login_modal_footer_actions">
                     <p className = "login_modal_password_forgot">비밀번호를 잊으셨나요?</p>
                     <div className = "login_modal_signup_container">
